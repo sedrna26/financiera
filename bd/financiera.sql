46204834305e23b8f333971b17320e4b0259ae39
Andres Rodriguez <sedrna26@gmail.com>
2025-03-20 16:12:09 -0300

    Update financiera.sql

--- bd/financiera.sql
+++ bd/financiera.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-03-2025 a las 22:32:25
+-- Tiempo de generación: 20-03-2025 a las 20:11:30
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -52,7 +52,7 @@ INSERT INTO `clientes` (`id_cliente`, `nombre`, `apellido`, `dni`, `domicilio`, 
 (8, 'Rosa Paola ', 'Fernandez', '26557734', 'B° TERESA DE CALCUTA, MZA F CASA 17, VA. ABERASTAIN, POCITO, SAN JUAN', '2645251067', 'Inactivo'),
 (9, 'Carmen Nelly ', 'Mercado', '17166756', 'B° HUARPES MZA H CASA 17, POCITO, SAN JUAN', '2644840485', 'Activo'),
 (10, 'Joana Beatriz ', 'Carrizo Mercado', '42645141', 'B° HUARPES MZA H CASA 17, POCITO, SAN JUAN', '2645012637', 'Inactivo'),
-(11, 'Micaela Alejandra', 'Carrizo Mercado', '51195824', 'B° HUARPES MZA H CASA 17, POCITO, SAN JUAN', '2645407957', 'Inactivo'),
+(11, 'Micaela Alejandra', 'Carrizo Mercado', '51195824', 'B° HUARPES MZA H CASA 17, POCITO, SAN JUAN', '2645407957', 'Activo'),
 (12, 'Miguel Angel ', 'Sarmiento', '22935648', 'ABERASTAIN SUR 287, B° PÉREZ HERNANDEZ, RIVADAVIA, SAN JUAN', '2645407957', 'Inactivo'),
 (13, 'Genesis Vanesa', 'Olmedo Saldaño', '39008091', 'MZA T CASA 2, SECTOR II OESTE, B° SIERRAS DEL MARQUESADO, RIVADAVIA, SAN JUAN', '2646123496', 'Inactivo'),
 (14, 'Maria Florencia', 'Gil', '40962859', 'DAVID CHAVEZ E/ CALLE 10 Y 11, LOTE 37, Vª ABERASTAIN, POCITO, SAN JUAN', '0', 'Inactivo'),
@@ -68,7 +68,8 @@ INSERT INTO `clientes` (`id_cliente`, `nombre`, `apellido`, `dni`, `domicilio`, 
 (24, 'Liliana Rosario', 'Balmaceda', '18594620', 'MZA P CASA 8 S/N, B° CERRO GRANDE, POCITO, SAN JUAN', '2645674356', 'Inactivo'),
 (25, 'Tamara Caren', 'Rios Olguin', '44249164', 'B° SAN JOSE MZA J CASA 6, RAWSON, SAN JUAN', '2645530411', 'Inactivo'),
 (26, 'Emanuel Alejandro', 'Espina', '28967961', 'MZA C CASA 16, B° CONJUNTO 8, RAWSON, SAN JUAN', '2646110163', 'Inactivo'),
-(27, 'Eliana Valeria', 'Olguin', '32007497', 'B° HUARPES, MZA G CASA 22, POCITO, SAN JUAN', '2643176337', 'Inactivo');
+(27, 'Eliana Valeria', 'Olguin', '32007497', 'B° HUARPES, MZA G CASA 22, POCITO, SAN JUAN', '2643176337', 'Inactivo'),
+(30, 'Jorge Cesar', 'Bacil', '11.142.487', 'DEVOTO OESTE 390, VILLA KRAUSE, RAWSON, SAN JUAN', '2644058489', 'Activo');
 
 -- --------------------------------------------------------
 
@@ -95,7 +96,9 @@ INSERT INTO `creditos` (`id`, `cliente_id`, `monto`, `cuotas`, `fecha_inicio`, `
 (4, 1, 100000.00, 1, '2025-03-21', '2025-03-21', 'Activo', 'mensual'),
 (5, 4, 100000.00, 1, '2025-04-12', '2025-04-12', 'Activo', 'mensual'),
 (6, 9, 100000.00, 1, '2025-04-12', '2025-05-12', 'Activo', 'mensual'),
-(7, 15, 200000.00, 3, '2025-03-05', '2025-05-05', 'Activo', 'mensual');
+(7, 15, 200000.00, 3, '2025-03-05', '2025-05-05', 'Activo', 'mensual'),
+(8, 11, 150000.00, 2, '2025-04-18', '2025-06-18', 'Activo', 'mensual'),
+(9, 30, 1060000.00, 1, '2025-04-02', '2025-04-02', 'Activo', 'quincenal');
 
 -- --------------------------------------------------------
 
@@ -144,13 +147,13 @@ ALTER TABLE `pagos`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_cliente` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `creditos`
 --
 ALTER TABLE `creditos`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restricciones para tablas volcadas
